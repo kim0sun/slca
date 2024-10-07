33d8859985724c7fc53c474ae0f3b57ecef3343e
kim0sun <kim0sun@korea.ac.kr>
2024-10-07 15:26:58 +0900

    slca test

--- tests/testthat/test-slca.R
+++ tests/testthat/test-slca.R
@@ -4,3 +4,7 @@ test_that("parenthesis/bracket identical", {
 test_that("stop without number works", {
    expect_error(slca(l ~ a + b + c))
 })
+test_that("stop if there are multiple parents", {
+   expect_error(slca(l[3] ~ a + b + c,
+                     l2[3] ~ a + b + c))
+})
