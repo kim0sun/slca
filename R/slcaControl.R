6ccba9ced391d21620a0aad075754544ce2957e4
kim0sun <kim0sun@korea.ac.kr>
2024-10-22 17:09:03 +0900

    control default changed

--- R/slcaControl.R
+++ R/slcaControl.R
@@ -1,12 +1,12 @@
 #' Control Parameters for slca Estimation
 #'
-#' @param em.iterlim an integer specifying maximum number of iterations allowed for EM algorithm. Default is 3000.
-#' @param em.tol a numeric value setting tolerance for the convergence of EM algorithm. Default is 1e-6.
+#' @param em.iterlim an integer specifying maximum number of iterations allowed for EM algorithm. Default is 5000.
+#' @param em.tol a numeric value setting tolerance for the convergence of EM algorithm. Default is 1e-8.
 #' @param nlm.iterlim an integer specifying maximum number of iterations allowed for estimation with \code{nlm} function. Default is 1000.
-#' @param nlm.tol a numeric value setting tolerance for the convergence of \code{nlm} function. Default is 1e-8.
+#' @param nlm.tol a numeric value setting tolerance for the convergence of \code{nlm} function. Default is 1e-10.
 #' @param init.param initial parameters.
 #' @param nrep number of trials. Default is 1.
-#' @param test.iter an integer specifying maximum number of iterations allowed for testing parameters. Default is 200
+#' @param test.iter an integer specifying maximum number of iterations allowed for testing parameters. Default is 500
 #' @param na.rm a logical value indicating whether to remove observations including missing values (NA)
 #' @param verbose a logical value indicating whether to display progress updates during the estimation process
 #'
@@ -16,10 +16,9 @@
 #'
 #' @export
 slcaControl <- function(
-   em.iterlim = 3000, em.tol = 1e-6,
-   nlm.iterlim = 1000, nlm.tol = 1e-8,
-   init.param = NULL,
-   nrep = 1, test.iter = 200,
+   em.iterlim = 5000, em.tol = 1e-8,
+   nlm.iterlim = 1000, nlm.tol = 1e-10,
+   init.param = NULL, nrep = 1, test.iter = 500,
    na.rm = FALSE, verbose = FALSE
 ) {
    ctrl <- list(
